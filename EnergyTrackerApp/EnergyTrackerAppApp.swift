9321c447c6961dddf1b1a836891c48dbc13e5dee
Melissa Breguez <melissa.breguez@gmail.com>
2023-11-29 18:32:58 -0700

    Initial Commit

---
+++ EnergyTrackerApp/EnergyTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnergyTrackerAppApp.swift
+//  EnergyTrackerApp
+//
+//  Created by Melissa Breguez on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct EnergyTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
